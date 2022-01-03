748d92eb43b230835960a6e01af17b978f4c1af6
Sritiman Adak <adaksritiman24@gmail.com>
2022-01-03 10:44:45 +0530

    appointment changed

--- odas/odas/settings.py
+++ odas/odas/settings.py
@@ -76,10 +76,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'odas.wsgi.application'
 ASGI_APPLICATION = 'odas.asgi.application'
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels.layers.InMemoryChannelLayer"
+#     }
+# }
+
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
 }
 
 
--- odas/static/chatting/js/main.js
+++ odas/static/chatting/js/main.js
@@ -1,5 +1,31 @@
 console.log("working");
 
+// var ICE_config= {
+//     'iceServers': [
+//         {url:'stun:stun01.sipphone.com'},
+//         {url:'stun:stun.ekiga.net'},
+//         {url:'stun:stun.fwdnet.net'},
+//         {url:'stun:stun.ideasip.com'},
+//         {url:'stun:stun.iptel.org'},
+//         {url:'stun:stun.rixtelecom.se'},
+//         {url:'stun:stun.schlund.de'},
+//         {url:'stun:stun.l.google.com:19302'},
+//         {url:'stun:stun1.l.google.com:19302'},
+//         {url:'stun:stun2.l.google.com:19302'},
+//         {url:'stun:stun3.l.google.com:19302'},
+//         {url:'stun:stun4.l.google.com:19302'},
+//         {url:'stun:stunserver.org'},
+//         {url:'stun:stun.softjoys.com'},
+//         {url:'stun:stun.voiparound.com'},
+//         {url:'stun:stun.voipbuster.com'},
+//         {url:'stun:stun.voipstunt.com'},
+//         {url:'stun:stun.voxgratia.org'},
+//         {url:'stun:stun.xten.com'},
+//     ]
+//   }
+
+var ICE_config=null
+
 var labelUsername = document.querySelector('#label-username');
 var usernameInput = document.querySelector('#username');
 var btnJoin = document.querySelector('#btn-join');
@@ -63,7 +89,7 @@ btnJoin.addEventListener('click', ()=>{
     labelUsername.innerHTML = username;
 
     var loc = window.location;
-    var wsStart = "ws://";
+    var wsStart = "wss://";
     
     if(loc.protocol== "https"){
         wsStart = "wss://";
@@ -160,7 +186,7 @@ function sendSignal(action, message){
 
 function createOffer(peerUsername, receiver_channel_name){
 
-    var peer = new RTCPeerConnection(null); //two devices in the same network
+    var peer = new RTCPeerConnection(ICE_config); //two devices in the same network
 
     addLocalTracks(peer); // take local audio and video tracks and add to the peer(RTCPeerConnection)
 
@@ -208,7 +234,7 @@ function createOffer(peerUsername, receiver_channel_name){
 
 function createAnswerer(offer, peerUsername, receiver_channel_name){
 
-    var peer = new RTCPeerConnection(null); //two devices in the same network
+    var peer = new RTCPeerConnection(ICE_config); //two devices in the same network
 
     addLocalTracks(peer); // take local audio and video tracks and add to the peer(RTCPeerConnection)
 
