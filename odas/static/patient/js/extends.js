ea211a94eb56df4ef1d88e1459fca18e0ce97d46
Sritiman Adak <adaksritiman24@gmail.com>
2022-02-20 21:07:49 +0530

    minimum date for appointment set to current date

--- odas/static/patient/js/extends.js
+++ odas/static/patient/js/extends.js
@@ -1,1 +1,11 @@
 console.log("Extends working");
+
+
+const setMinDate = (event)=>{
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+    today = yyyy + '-' + mm + '-' + dd;
+    event.target.min = today;
+}
