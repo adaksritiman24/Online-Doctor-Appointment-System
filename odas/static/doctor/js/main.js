e4c731d0ebc2b47a38aee6d21cfb2669ed4a661f
Sritiman Adak <adaksritiman24@gmail.com>
2022-04-05 11:49:52 +0530

    report/presc. upload/view tested, css updated

--- odas/static/doctor/js/main.js
+++ odas/static/doctor/js/main.js
@@ -35,24 +35,24 @@ const showReportsModal = (data)=> {
     finally {
 
         const reports = data.map(report=> `
-        <div>
+        <div class="report-display-each">
         <b>${report.name}</b>
         <a href=${document.location.origin+`/edit/patient/reports/${report.id}`} target="_blank">View this Report</a>
         </div>
         `);
 
-
+        const reportTemplate = reports.join(" ");
 
 
         var modalContainer = document.getElementById('report-modal-content');
         modalContainer.innerHTML =
         `
         <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Reports</h5>
+            <h5 class="modal-title" id="reportsModalLabel">Reports</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-            ${reports}
+            ${reportTemplate}
         </div>
         <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
